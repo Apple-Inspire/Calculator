dd331fbac40a3d1040ffd594f354e7bfb4fea25f
Yash Nayak <yugn27@gmail.com>
2018-09-06 23:29:49 +0530

    Add files via upload

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by yashn on 01/09/18.
+//  Copyright © 2018 yashn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var value: UITextField!
+    
+    @IBOutlet weak var intrest: UITextField!
+    
+    @IBOutlet weak var year: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+    @IBAction func calbtn(_ sender: Any) {
+    }
+    
+    
+    
+
+}
+
+
